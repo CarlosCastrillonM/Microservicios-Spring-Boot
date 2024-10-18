e67aa0880d7b0d311e05e791767d7773facf125d
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-10-18 03:48:21 -0500

    fixed class ReservaServiceImp

--- Reservation/src/main/java/com/example/rsc/service/impl/ReservaServiceImp.java
+++ Reservation/src/main/java/com/example/rsc/service/impl/ReservaServiceImp.java
@@ -1,51 +1,71 @@
 package com.example.rsc.service.impl;
 
 import com.example.rsc.dto.ReservaDto;
+import com.example.rsc.entity.Reserva;
+import com.example.rsc.mapper.ReservaMapper;
 import com.example.rsc.repository.ReservaRepository;
 import com.example.rsc.service.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ReservaServiceImp implements ReservaService {
-
     private final ReservaRepository reservaRepository;
+    private final ReservaMapper reservaMapper;
 
     @Autowired
-    public ReservaServiceImp(ReservaRepository reservaRepository) {
+    public ReservaServiceImp(ReservaRepository reservaRepository, ReservaMapper reservaMapper) {
         this.reservaRepository = reservaRepository;
+        this.reservaMapper = reservaMapper;
     }
 
-    @Override
     public List<ReservaDto> findAll() {
-        return List.of();
+        List<Reserva> reserva = reservaRepository.findAll();
+        List<ReservaDto> reservaDtos = new ArrayList<>();
+
+        for (Reserva reservaDto : reserva) {
+            reservaDtos.add(reservaMapper.toDto(reservaDto));
+        }
+        return reservaDtos;
     }
 
     @Override
     public Optional<ReservaDto> find(Integer id) {
-        return Optional.empty();
+        Optional<Reserva> reserva = reservaRepository.findById(id);
+        Optional<ReservaDto> reservaDtos = Optional.empty();
+
+        reservaDtos = reserva.map(reservaMapper::toDto);
+        return reservaDtos;
     }
 
     @Override
     public ReservaDto create(ReservaDto reservaDto) {
-        return null;
+        Reserva entidadReserva = new Reserva();
+
+        entidadReserva = reservaMapper.toEntity(reservaDto);
+        entidadReserva = reservaRepository.save(entidadReserva);
+
+        reservaDto = reservaMapper.toDto(entidadReserva);
+        return reservaDto;
     }
 
     @Override
     public Optional<ReservaDto> update(Integer id, ReservaDto newReservaDto) {
-        return Optional.empty();
+        return reservaRepository.findById(id).map(ReservaInDB -> {
+            ReservaInDB = reservaMapper.toEntity(newReservaDto);
+            ReservaInDB = reservaRepository.save(ReservaInDB);
+
+            return reservaMapper.toDto(ReservaInDB);
+        })  ;
     }
 
     @Override
     public void delete(Integer id) {
-
+        reservaRepository.deleteById(id);
     }
 
-    @Override
-    public List<ReservaDto> findByName(String name) {
-        return List.of();
-    }
 }
