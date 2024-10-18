47f866137b5420395c36540b82fe9b79b578ccc8
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-10-18 03:24:39 -0500

    Entity Reserva modified

--- Reservation/src/main/java/com/example/rsc/entity/Reserva.java
+++ Reservation/src/main/java/com/example/rsc/entity/Reserva.java
@@ -1,30 +1,28 @@
 package com.example.rsc.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Entity
-@Table(name = "reservas")
-@Getter
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
 public class Reserva {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "user_id")
     private Integer usuarioId;
 
-    @Column(name = "oferta_id")
     private Integer ofertaId;
 
-    @Column(name = "fecha_reserva")
     private LocalDateTime fechaReserva;
 
-    @Column(name = "total_pago")
     private Double totalPago;
 
 }
