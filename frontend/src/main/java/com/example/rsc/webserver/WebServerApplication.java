6ba35552cb82ba8b87cf0c04863aca288d8ca6ab
CarlosCastrillonM <carlangas.c.m.4@gmail.com>
2024-10-20 23:17:48 -0500

    small adjustments

--- frontend/src/main/java/com/example/rsc/webserver/WebServerApplication.java
+++ frontend/src/main/java/com/example/rsc/webserver/WebServerApplication.java
@@ -8,6 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebServerApplication.class, args);
-        HomeController homeController = new HomeController();
     }
 }
